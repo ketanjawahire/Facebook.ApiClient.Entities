89b6c36c1b68a70d3fe7ce3d5d1502ace1c916d6
David Stephens <david.stephens@chancellors.co.uk>
2023-01-24 14:43:08 +0000

    Add new OptimizationGoal entities

--- Facebook.ApiClient.Entities/Enumerations/OptimizationGoal.cs
+++ Facebook.ApiClient.Entities/Enumerations/OptimizationGoal.cs
@@ -14,9 +14,24 @@ namespace Facebook.ApiClient.Entities.Enumerations
         NONE,
 
         /// <summary>
+        /// APP_DOWNLOADS
+        /// </summary>
+        APP_DOWNLOADS,
+
+        /// <summary>
         /// Optimize for people more likely to install your app.
         /// </summary>
         APP_INSTALLS,
+
+        /// <summary>
+        /// Optimizes for people more likely to install your app and make a conversion on your site.
+        /// </summary>
+        APP_INSTALLS_AND_OFFSITE_CONVERSIONS,
+
+        /// <summary>
+        /// Optimize for people more likely to remember seeing your ads.
+        /// </summary>
+        AD_RECALL_LIFT,
 
         /// <summary>
         /// Optimize to reach the most number of users who are likely to spend at least a minimum amount of time on the image or video
@@ -27,6 +42,18 @@ namespace Facebook.ApiClient.Entities.Enumerations
         ///  Optimize for people more likely to click anywhere in the ad.
         /// </summary>
         CLICKS,
+
+        /// <summary>
+        ///  Directs ads to people more likely to have a conversation with the business.
+        /// </summary>
+        CONVERSATIONS,
+
+        /// <summary>
+        ///  Optimize for retention, which reaches people who are most likely to return to the app and open it again during a given time frame after installing. 
+        ///  You can choose either two days, meaning the app is likely to be reopened between 24 and 48 hours after installation; or seven days, meaning the app 
+        ///  is likely to be reopened between 144 and 168 hours after installation.
+        /// </summary>
+        DERIVED_EVENTS,
 
         /// <summary>
         /// Optimize for people more likely to take a particular action in your app 
@@ -49,6 +76,16 @@ namespace Facebook.ApiClient.Entities.Enumerations
         IMPRESSIONS,
 
         /// <summary>
+        /// Optimize for people who are most likely to click on and load your chosen landing page.
+        /// </summary>
+        LANDING_PAGE_VIEWS,
+
+        /// <summary>
+        /// IN_APP_VALUE
+        /// </summary>
+        IN_APP_VALUE,
+
+        /// <summary>
         /// Optimize for people more likely to fill out a lead generation form.
         /// </summary>
         LEAD_GENERATION,
@@ -57,6 +94,16 @@ namespace Facebook.ApiClient.Entities.Enumerations
         /// Optimize for people more likely to click in the link of the ad. 
         /// </summary>
         LINK_CLICKS,
+
+        /// <summary>
+        /// MESSAGING_PURCHASE_CONVERSION 
+        /// </summary>
+        MESSAGING_PURCHASE_CONVERSION,
+
+        /// <summary>
+        /// MESSAGING_APPOINTMENT_CONVERSION 
+        /// </summary>
+        MESSAGING_APPOINTMENT_CONVERSION,
 
         /// <summary>
         /// Optimize for people more likely to claim the offer.
@@ -84,6 +131,16 @@ namespace Facebook.ApiClient.Entities.Enumerations
         POST_ENGAGEMENT,
 
         /// <summary>
+        /// Optimize for people who are likely to call the advertiser.
+        /// </summary>
+        QUALITY_CALL,
+
+        /// <summary>
+        /// Optimize for people who are likely to have a deeper conversation with advertisers after lead submission.
+        /// </summary>
+        QUALITY_LEAD,
+
+        /// <summary>
         /// Optimize to reach the most unique users of each day or interval specified in <see cref="AdSet.FrequencyControlSpec"/>
         /// </summary>
         REACH,
@@ -95,20 +152,24 @@ namespace Facebook.ApiClient.Entities.Enumerations
         SOCIAL_IMPRESSIONS,
 
         /// <summary>
+        /// Optimize delivery of your ads to people who are more likely to play your ad to completion, or play it for at least 15 seconds.
+        /// </summary>
+        THRUPLAY,
+
+        /// <summary>
+        /// Optimize for maximum total purchase value within the specified attribution window.
+        /// </summary>
+        VALUE,
+
+        /// <summary>
         /// Optimize for people more likely to watch videos
         /// </summary>
         VIDEO_VIEWS,
 
         /// <summary>
-        /// APP_DOWNLOADS
+        /// Optimize for visits to the advertiser's Instagram profile.
         /// </summary>
-        APP_DOWNLOADS,
-
-        /// <summary>
-        /// LANDING_PAGE_VIEWS
-        /// </summary>
-        LANDING_PAGE_VIEWS,
-
+        VISIT_INSTAGRAM_PROFILE,
 
     }
 }
